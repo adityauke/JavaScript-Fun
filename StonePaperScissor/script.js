3ca7598522afb1825c3ee7518c2fe12d0c5b511d
Aditya Uke <adityauke4141@gmail.com>
2023-08-11 19:13:49 +0530

    Update script.js

--- StonePaperScissor/script.js
+++ StonePaperScissor/script.js
@@ -1,45 +1,121 @@
-const moves = ["Rock", "Paper", "Scissors"];
-let playerScore = 0;
+let userScore = 0;
 let computerScore = 0;
 
-const computerPlay = () => moves[Math.floor(Math.random() * moves.length)];
+const userScoreSpan = document.getElementById("user-score");
+const computerScoreSpan = document.getElementById("computer-score");
+const scoresDiv = document.querySelector(".scores");
+const resultDiv = document.querySelector(".result > p");
+const resetButton = document.getElementById("reset-button")
 
-const playRound = (playerSelection, computerSelection) => {
-    playerSelection = playerSelection.toLowerCase();
-    const playerIndex = moves.indexOf(playerSelection);
-    const computerIndex = moves.indexOf(computerSelection);
+const rockDiv = document.getElementById("rock");
+const paperDiv = document.getElementById("paper");
+const scissorsDiv = document.getElementById("scissors");
 
-    if (playerIndex === computerIndex) {
-        return "Tie";
-    } else if (
-        (playerIndex === 0 && computerIndex === 2) ||
-        (playerIndex === 1 && computerIndex === 0) ||
-        (playerIndex === 2 && computerIndex === 1)
-    ) {
-        playerScore++;
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else {
-        computerScore++;
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    }
+const main = () => {
+  rockDiv.addEventListener('click', () => game("rock"));
+
+  paperDiv.addEventListener('click', () => game("paper"));
+
+  scissorsDiv.addEventListener('click', () => game("scissors"));
+
+  resetButton.addEventListener('click', () => resetScores());
 };
 
-const updateResult = (message) => {
-    const resultElement = document.querySelector(".result");
-    resultElement.textContent = message;
+const getComputerChoice = () => {
+  const choiceList = ["rock", "paper", "scissors"];
+  const randomNumber = Math.floor(Math.random()*3);
+  return (choiceList[randomNumber]);
 };
 
-const updateScores = () => {
-    console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`);
+
+// Convert lowercase choice to uppercase
+const convertToUp = (word) => {
+  switch(word) {
+    case "rock":
+      return "Rock";
+      break;
+    case "paper":
+      return "Paper";
+      break;
+    case "scissors":
+      return "Scissors";
+      break;
+  }
 };
 
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        const playerSelection = button.textContent;
-        const computerSelection = computerPlay();
-        const message = playRound(playerSelection, computerSelection);
-        updateResult(message);
-        updateScores();
-    });
-});
+const win = (userChoice, computerChoice) => {
+  userScore++;
+  userScoreSpan.innerHTML = userScore;
+  const randomWin = ["beats", "smashes", "destroys"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  const winEmojis = ["😁", "💃🏽", "👏🏽", "😅", "😎", "🙌🏽"]
+  const randomNumberEmoji = Math.floor(Math.random() * 6);
+
+  resultDiv.innerHTML = `${convertToUp(userChoice)} ${randomWin[randomNumber]} ${convertToUp(computerChoice)}. You win! ${winEmojis[randomNumberEmoji]}`;
+
+  document.getElementById(userChoice).classList.add('win-border')
+  setTimeout(() => document.getElementById(userChoice).classList.remove('win-border'), 600);
+};
+
+const lose = (userChoice, computerChoice) => {
+  computerScore++;
+  computerScoreSpan.innerHTML = computerScore;
+  const randomWin = ["beats", "smashes", "destroys"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  const loseEmojis = ["😩", "😾", "💩", "😭", "😡", "🤨", "🤦🏽"]
+  const randomNumberEmoji = Math.floor(Math.random() * 7);
+  resultDiv.innerHTML = `${convertToUp(computerChoice)} ${randomWin[randomNumber]} ${convertToUp(userChoice)}. You lose! ${loseEmojis[randomNumberEmoji]}`;
+
+  document.getElementById(userChoice).classList.add('lose-border');
+  setTimeout(() => document.getElementById(userChoice).classList.remove('lose-border'), 600);
+
+};
+
+const tie = (userChoice, computerChoice) => {
+  const tieEmojis = ["🤯", "😱", "🙈", "🧐", "🙀", "🙃"];
+  const randomNumberEmoji = Math.floor(Math.random() * 6);
+  resultDiv.innerHTML = `${convertToUp(computerChoice)} matches ${convertToUp(userChoice)}. It's a tie! ${tieEmojis[randomNumberEmoji]}`;
+
+  document.getElementById(userChoice).classList.add('tie-border');
+  setTimeout(() => document.getElementById(userChoice).classList.remove('tie-border'), 600);
+};
+
+
+
+
+const game = (userChoice) => {
+  const computerChoice = getComputerChoice();
+
+  switch (userChoice + computerChoice) {
+    case "paperrock":
+    case "rockscissors":
+    case "scissorspaper":
+      win(userChoice, computerChoice);
+      break;
+
+    case "rockpaper":
+    case "scissorsrock":
+    case "paperscissors":
+      lose(userChoice, computerChoice);
+      break;
+
+    case "rockrock":
+    case "paperpaper":
+    case "scissorsscissors":
+      tie(userChoice, computerChoice);
+      break;
+  }
+
+};
+
+
+const resetScores = () => {
+  computerScore = 0;
+  computerScoreSpan.innerHTML = computerScore
+  userScore = 0;
+  userScoreSpan.innerHTML = userScore;
+};
+
+
+
+main();
